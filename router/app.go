c6e2bc05605a9ddc0b9f364268bfe787fcaf473a
tiejiaxiaobao <2922035952@qq.com>
2024-01-06 23:48:24 +0800

    The swagger configuration is completed, and the basic environment configuration is completed.

--- router/app.go
+++ router/app.go
@@ -1,12 +1,26 @@
 package router
 
 import (
+	"OJ/docs"
 	"OJ/service"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
+
+	// programatically set swagger info
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "This is a sample server Petstore server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "petstore.swagger.io"
+	docs.SwaggerInfo.BasePath = "/v2"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
+	// Swagger 配置
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.GET("/app", service.Test)
 	r.GET("/get_question", service.GetQuestionList)
--- service/question_service.go
+++ service/question_service.go
@@ -6,6 +6,15 @@ import (
 	"net/http"
 )
 
+// GetQuestionList godoc
+// @Summary      查询Question问题列表
+// @Description	 test
+// @Tags         question
+// @Accept       json
+// @Produce      json
+// @Param        id   path      int  true  "Question ID"
+// @Success      200  {string} json "{"code" : "200", "msg" : "", "data" : ""}"
+// @Router       /question/{id} [get]
 func GetQuestionList(c *gin.Context) {
 	dao.GetQuestionList()
 	c.String(http.StatusOK, "success")
