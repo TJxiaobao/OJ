38e72b644b04af20ff3166bb34579b398421d452
tiejiaxiaobao <2922035952@qq.com>
2024-02-06 22:44:23 +0800

    Use email version v1

--- service/user_service.go
+++ service/user_service.go
@@ -12,6 +12,7 @@ import (
 	"github.com/TJxiaobao/OJ/utils/token"
 	"github.com/TJxiaobao/OJ/utils/uuid"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/gomail.v2"
 	"log"
 	"math/rand"
 	"net/http"
@@ -193,11 +194,35 @@ func SendCode(c *gin.Context) {
 	}
 
 	code := createCode()
-	err := dao.InitRedis().Set(c, cmd.Email, code, time.Second*300) // 设置时间为 5 分钟
+	err := dao.InitRedis().Set(c, cmd.Email, code, time.Second*300).Err() // 设置时间为 5 分钟
 	if err != nil {
 		log.Print("redis set error", err)
 		restapi.FailedWithStatus(c, nil, 500)
 		return
 	}
+
+	sender := "test@qq.com"      //发送者qq邮箱
+	authCode := "auth"           //qq邮箱授权码
+	mailTitle := "OJ验证码"         //邮件标题
+	mailBody := "注册的验证码为" + code //邮件内容,可以是html
+
+	//接收者邮箱列表
+	mailTo := []string{
+		cmd.Email,
+	}
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", sender)       //发送者腾讯邮箱账号
+	m.SetHeader("To", mailTo...)      //接收者邮箱列表
+	m.SetHeader("Subject", mailTitle) //邮件标题
+	m.SetBody("text/html", mailBody)  //邮件内容,可以是html
+
+	//发送邮件服务器、端口、发送者qq邮箱、qq邮箱授权码
+	//服务器地址和端口是腾讯的
+	d := gomail.NewDialer("smtp.qq.com", 587, sender, authCode)
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
+
 	restapi.Success(c, "send code success !")
 }
---
+++ utils/email/email.go
@@ -0,0 +1,18 @@
+package email
+
+type MailboxConf struct {
+	// 邮件标题
+	Title string
+	// 邮件内容
+	Body string
+	// 收件人列表
+	RecipientList []string
+	// 发件人账号
+	Sender string
+	// 发件人密码，QQ邮箱这里配置授权码
+	SPassword string
+	// SMTP 服务器地址， QQ邮箱是smtp.qq.com
+	SMTPAddr string
+	// SMTP端口 QQ邮箱是25
+	SMTPPort int
+}
